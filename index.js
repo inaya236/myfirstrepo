18a3e4d32a6b5aa11902d0624b54d433bc8ed89e
inaya236 <inayaaa269@gmail.com>
2024-10-17 19:58:41 +0530

    new msg added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import React from 'react'
+// import React from 'react'
+// export default function Home() {
+//   return (
+//     <div>Home</div>
+//   )
+// }
+console.log("new commit msg");
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
-}
